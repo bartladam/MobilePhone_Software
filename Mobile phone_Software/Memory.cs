0359fda5d978113c93d308b3e87bc5bd72370431
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-16 15:49:47 +0200

    memory rewritten

--- Mobile phone_Software/Memory.cs
+++ Mobile phone_Software/Memory.cs
@@ -8,13 +8,24 @@ namespace Mobile_phone_Software
 {
     internal class Memory
     {
-        private const int sizeMemory = 500; // MB
+        private const int sizeMemory = 1500; // MB
         public int memoryStatus { get; set; }
         public Software software { get; set; }
+        public List<object> application { get; set; }
         public Memory(Software software)
         {
-            //memoryStatus = software.size
+            application = new List<object>();
+            application.Add(new Settings());
+            application.Add(new GooglePlay());
+            application.Add(new Telephone());
+            application.Add(new SMS());
             this.software = software;
+            memoryStatus = software.sizeSoftware; // add new settings size, google play etc
+        }
+        public void AddApp(Application application)
+        {
+            this.application.Add(application);  
+            ///memoryStatus += application.size
         }
         public string StatusMemory() => string.Format("Memory is filled up:\n{0}/{1} ({2})", memoryStatus, sizeMemory, (memoryStatus/sizeMemory)*100);
 
