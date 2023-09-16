932a0d2a0b6dbb4ecb1ea8969b65defa6be4f5f3
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-16 14:17:37 +0200

    memory class without connection with other classes

--- Mobile phone_Software/Memory.cs
+++ Mobile phone_Software/Memory.cs
@@ -8,5 +8,15 @@ namespace Mobile_phone_Software
 {
     internal class Memory
     {
+        private const int sizeMemory = 500; // MB
+        public int memoryStatus { get; set; }
+        public Software software { get; set; }
+        public Memory(Software software)
+        {
+            //memoryStatus = software.size
+            this.software = software;
+        }
+        public string StatusMemory() => string.Format("Memory is filled up:\n{0}/{1} ({2})", memoryStatus, sizeMemory, (memoryStatus/sizeMemory)*100);
+
     }
 }
