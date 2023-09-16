f2dd36eff026f49ad370b95fb474fd4b3e1a4669
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-16 13:59:51 +0200

    mobile phone withou connection complete

--- Mobile phone_Software/MobilePhone.cs
+++ Mobile phone_Software/MobilePhone.cs
@@ -8,5 +8,26 @@ namespace Mobile_phone_Software
 {
     internal class MobilePhone
     {
+        private long serialNumber { get; set; }
+        private string phoneName { get; set; }
+        private Random randomSerialNumber { get; set; }
+        private Memory memory { get; set; }
+
+        public MobilePhone(string phoneName, Memory memory)
+        {
+            this.phoneName = phoneName;
+            randomSerialNumber = new Random();
+            serialNumber = randomSerialNumber.Next();
+            this.memory = memory;
+            OpenSoftware();
+        }
+        public void OpenSoftware()
+        {
+            // memory select software 
+        }
+        public override string ToString()
+        {
+            return serialNumber.ToString();
+        }
     }
 }
